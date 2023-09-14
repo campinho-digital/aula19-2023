ec3c37b99b3b3d52210d416a5507cc443b7b3b22
Julli Mayanne <jullimayanne9@gmail.com>
2023-09-14 20:08:07 -0300

    criando arquivos

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+/*
+Declara uma variável "i" e atribui o valor 0 a ela
+Declara uma variável "j" sem atribuir valor
+Atribui o valor 10 à variável "i"
+Atribui a string "Mundo" à variável "j"
+*/
 
-const value = 4500
-const valueWith = (value * 20)/ 100
-const result = value - valueWith
 
-console.log(result)
+/*
+let x = 5; // Declara uma variável "x" e atribui o valor 5 a ela
+let y; // Declara uma variável "y" sem atribuir valor
+x = 10; // Atribui o valor 10 à variável "x"
+y = "Olá"; // Atribui a string "Olá" à variável "y"
+*/
+
+
+/*
+
+const PI = 3.14; // Declara uma constante com valor 3.14
+const nome = "João"; // Declara uma constante com valor "João"
+const array = [1, 2, 3]; // Declara uma constante que armazena um array
+
+// Tentativa de atribuir um novo valor à constante PI:
+PI = 3.1415; // Gera um erro, pois não é possível reatribuir valor a uma constante
+
+
+2 - João comecou a trabalhar como desenvolvedor mas precisa de comprar um
+computador melhor. Achou um no preço de R$4.500,00 que é o valor que pode gastar
+no momento. Se ele comprar online vai ter um desconto de 20%.
+Calcule o preço final que João pagará com o desconto.
+Entrada:
+valor inicial do computador
+Processamento:
+Calcular o desconto no valor inicial
+Saída:
+Resultado do preço final no console.
+*/
+
+
+
 
