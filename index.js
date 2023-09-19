328a63ebec5478949a722a45e2dad2a77ce6a95d
PABLOHLS3 <pablo.henrrique5844@gmail.com>
2023-09-19 20:29:14 -0300

    Novo Usuario

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+/*Crie três constantes: nomeCliente, idadeCliente e eMembro para armazenar informações sobre um cliente específico.
+- Certifique-se de usar o tipo de dado apropriado para cada constante (string, number, bool).
 
+- Agora, cada integrante do grupo precisará alterar os valores das constantes para representar outro funcionário
+e exibir as informações desse novo funcionário no console.
+
+- Assim que isso for feito, voce deverá criar uma branch e enviar as atualizacoes para a sua respectiva branch. 
+
+Feito isso, voce deverá criar um Pull request endereçado a `main/master` do projeto.*/
+
+
+const nomeCliente = "Bernado";
+const idadeCliente = 32;
+const eMembro = true;
+
+console.log(nomeCliente, idadeCliente, eMembro)
